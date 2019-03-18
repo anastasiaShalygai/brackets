90946a93269844e81cd571cb47be442e9680ae03
Nastya Ivanova <shalygai.a@gmail.com>
2019-03-18 18:42:54 +0300

    feat(brackets): validation of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+  let bracketsConfigSplit = bracketsConfig.toString().split(',').join('');
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
+  let  bracketsReg = new RegExp("[^" + escapeRegExp(bracketsConfigSplit) +"]", "g");
+  let strWithOnlyBrackets = str.replace(bracketsReg,"");
+
+  let previosState="";
+
+  while(strWithOnlyBrackets.length != previosState.length) {
+    previosState = strWithOnlyBrackets;
+    bracketsConfig.forEach(function(item){
+      strWithOnlyBrackets = strWithOnlyBrackets.replace(item.join(""), "")
+    })
+  }
+  return !strWithOnlyBrackets;
 }
